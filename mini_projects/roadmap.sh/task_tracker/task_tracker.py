fc698b7e96090d2b0c9a42ae503e08298cc8d615
TacNap28 <matthewtlittle@gmail.com>
2025-01-02 17:59:04 +1000

    began work on first project

---
+++ mini_projects/roadmap.sh/task_tracker/task_tracker.py
@@ -0,0 +1,40 @@
+# 2/1/2025
+# Task Tracker project from https://roadmap.sh/projects/task-tracker
+
+"""
+Each task has the properties:
+	id
+	description
+	status
+	createdAt : Date and Time of creation of task
+	updatedAt : Date and Time of last update of task
+"""
+import json
+import os
+
+# User-Called Functions
+def add(description):
+	pass
+
+def update(id = 0, description = "", status = ""):
+ 	pass
+
+def delete(id = 0):
+	pass
+
+def mark_done(id = 0):
+	pass
+
+def mark_prog(id = 0):
+	pass
+
+def list_done():
+	pass
+
+def list_todo():
+	pass
+
+def list_prog():
+	pass
+
+
