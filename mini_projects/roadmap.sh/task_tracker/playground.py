f7f9a8ff2b25dd5958e630bd3019ca5d6fb9ee46
TacNap28 <matthewtlittle@gmail.com>
2025-01-06 15:42:46 +1000

    updated task tracker project

--- mini_projects/roadmap.sh/task_tracker/playground.py
+++ mini_projects/roadmap.sh/task_tracker/playground.py
@@ -1,38 +1,9 @@
-import json
-import datetime
+import task_tracker
 
-filepath = "tasks.json"
+task_tracker.list_tasks()
+task_tracker.add("New Task")
+task_tracker.update(5, "Updated Task")
+task_tracker.delete(5)
 
-# --- Helper Functions --- #
-def open_file():
-	try:
-		with open(filepath, 'r') as file:
-			data = json.load(file)
-	except FileNotFoundError:
-		data = {}
-	except json.JSONDecodeError:
-		data = {}
-	return data
+task_tracker.update_status(1)
 
-def write_file(data):
-	with open(filepath, 'w') as file:
-		json.dump(data, file, indent=2)
-
-def update(id, description, status = ""):
-	# Open the file
-	data = open_file()
-
-	# Place reference to task into variable
-	try:
-		task = data["tasks"][id-1]
-	except IndexError:
-		print("Invalid Task ID")
-
-
-	# Mutate the task description and updated time
-	task["description"] = description
-	task["updatedAt"] = "Updated!"
-	if status != "": task["status"] = status
-	
-	# Write to the file
-	write_file(data)
--- mini_projects/roadmap.sh/task_tracker/task_tracker.py
+++ mini_projects/roadmap.sh/task_tracker/task_tracker.py
@@ -38,8 +38,9 @@ def add(description):
 	data["tasks"].append(new_task)
 
 	write_file(data)
+	print("Task Added!")
 
-def update(id, description, status = ""):
+def update(id, description):
 	# Open the file
 	data = open_file()
 
@@ -55,28 +56,79 @@ def update(id, description, status = ""):
 
 	# Mutate the task description and updated time
 	task["description"] = description
-	task["updatedAt"] = "Updated!"
-	if status != "": task["status"] = status
+	update_time(task)
+	
+	# Write to the file
+	write_file(data)
+	print("Task Updated!")
+
+def update_status(id):
+	statuses = ["none", "To-Do", "In Progress", "Done"]
+	# Open file
+	data = open_file()
+
+	# Place task reference into a variable
+	try:
+		task = data["tasks"][id-1]
+	except IndexError:
+		print("Invalid Task ID")
+		return
+	except TypeError:
+		print("Invalid Task ID: Provide an Integer")
+		return
+
+	# Receive User Input
+	print("\nFor task: '" + task["description"]+"', " + task["status"])
+	print("Update to what?")
+	print("[1] - To-Do")
+	print("[2] - In Progress")
+	print("[3] - Done")
+	try:
+		status_code = int(input("> "))
+	except ValueError:
+		print("Input a number")
+		return
+	
+	# Change Status Accordingly
+	if status_code >= 1 and status_code <= 3:
+		task["status"] = statuses[status_code]
+	else: 
+		print("Input the right number")
+		return
+
+	# Update time
+	update_time(task)
+	
+	# Write to the file
+	write_file(data)
+	print("Status changed to " + statuses[status_code] + "!")
+
+def delete(id):
+	# Open the file
+	data = open_file()
+
+	# Place reference to task into variable
+	try:
+		task = data["tasks"][id-1]
+	except IndexError:
+		print("Invalid Task ID")
+		return
+	except TypeError:
+		print("Invalid Task ID: Provide an Integer")
+		return
+
+	removed_task = data["tasks"].pop(id-1)
+	print("Removed Task [ID : " + str(removed_task["id"]) + "] -",removed_task["description"])
+	
 	
 	# Write to the file
 	write_file(data)
 
-def delete(id = 0):
-	pass
-
-def mark_done(id = 0):
-	pass
-
-def mark_prog(id = 0):
-	pass
+def list_tasks(status = ""):
+	data = open_file()
+	print_list(data)
 
 def list_done():
-	pass
-
-def list_todo():
-	pass
-
-def list_prog():
 	pass
 
 # --- Helper Functions --- #
@@ -90,14 +142,28 @@ def open_file():
 		data = {}
 	return data
 
+# Currently prints JSON format
+# Change this to be more presentable
 def print_list(data):
-	print(data)
+	#json_formatted = json.dumps(data, indent=2)
+	#print(json_formatted)
+	line_break = "______________________________________________________"
+	print(line_break)
+	for task in (data["tasks"]):
+		
+		print("[ID :",str(task["id"]) +"]")
+		print(task["description"])
+		print("	",task["status"])
+		print("	","Created:",task["createdAt"])
+		print("	","Last Update:",task["updatedAt"])
+		print(line_break)
 
 def write_file(data):
 	with open(filepath, 'w') as file:
 		json.dump(data, file, indent=2)
 
-	print(data)
+def update_time(task):
+	task["updatedAt"] = "Updated!"
 
 
 
